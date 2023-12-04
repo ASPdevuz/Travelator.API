21314bd94ee2c538e2f709f51a198798303e7b51
Theakhmadjonov <ahmadjonovsobirjon24@gmail.com>
2023-12-04 15:39:20 +0300

    Create services #1

---
+++ Travelator.API/Service/ITicketService.cs
@@ -0,0 +1,14 @@
+﻿using Travelator.API.Dtos;
+using Travelator.API.Entities;
+
+namespace Travelator.API.Service
+{
+    public interface ITicketService
+    {
+        Task<List<Ticket>> GetTicket();
+        Task<Ticket> GetTicket(Guid id);
+        Task<Ticket> CreateTicket(CreateTicketDto newTicket);
+        Task<Ticket> UpdateTicket(UpdateTicketDto ticket);
+        Task<bool> DeleteTicket(Guid id);
+    }
+}
